db8c828cf9406a54d7b706428c988d31241900a5
Javi Granados <jjavigrana2@gmail.com>
2026-04-16 19:20:40 +0200

    a la mierda

--- extra-addons/modul_cinema/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import pelicula
-from . import director
-from . import actor
-from . import genere
--- extra-addons/modul_musica/models/artista.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class MusicaArtista(models.Model):
-    _name = 'musica.artista'
-    _description = 'Artista Musical'
-
-    nom = fields.Char(string='Nom', required=True)
-    datanaix = fields.Date(string='Data de Naixement')
